25d7460d02deb1a1ffa512bd6e8e853fa18b98ca
pnr-55 <39924@pranarai.ac.th>
2026-07-08 08:13:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-mport streamlit as st
+import streamlit as st
 import pandas as pd
 st.set_page_config(page_title="Knee AI", layout="centered")
 if 'user_data' not in st.session_state: st.session_state.user_data = None
