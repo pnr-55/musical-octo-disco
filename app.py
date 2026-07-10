4165250933ec6de6c164ad2d45ecbc1dabd432d6
pnr-55 <39924@pranarai.ac.th>
2026-07-10 11:32:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ if submitted:
     st.header("3-5. ผลการวิเคราะห์")
     if uploaded_file is not None:
         st.image(uploaded_file, caption="ภาพของคุณ", use_container_width=True)
-        angle = 165 # จำลองค่าการคำนวณ
+        angle = คำนวนจากภาพ # จำลองค่าการคำนวณ
         st.write(f"มุมความเบี่ยงเบนของขา: {angle}°")
         
         # 6. ประเมินผลและแนะนำ
